0f2af574129592685aeac204e4b29b379f33c44d
DoudiNCer <doudi@outlook.lv>
2023-02-22 15:39:18 +0800

    Chore: 降级 Go 至1.19以便兼容kitex

--- main.go
+++ main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"github.com/DodiNCer/kitex-ffmpeg/env"
 	kitex_ffmpeg "github.com/DodiNCer/kitex-ffmpeg/kitex_gen/kitex_ffmpeg/kitexffmpeg"
+	"github.com/cloudwego/kitex/server"
 	"log"
+	"net"
 )
+
+const ADDR = "0.0.0.0:9427"
 
 func main() {
 	env.Init()
-	svr := kitex_ffmpeg.NewServer(new(KitexFfmpegImpl))
+	addr, _ := net.ResolveTCPAddr("tcp", ADDR)
+	svr := kitex_ffmpeg.NewServer(new(KitexFfmpegImpl), server.WithServiceAddr(addr))
 
 	err := svr.Run()
 
